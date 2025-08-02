b405a85f520e14e88c0ae7366be2f6e5b4d64506
Wolphyung <deanwolphyungralaivao2004@gmail.com>
2025-08-02 10:16:01 +0300

    Initialisation du plugin

---
+++ mon-plugin-meteo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       Mon Plugin Météo
+ * Plugin URI:        https://github.com/votre-nom-utilisateur/mon-plugin-meteo
+ * Description:       Affiche la météo de l'utilisateur.
+ * Version:           1.0.0
+ * Author:            Votre Nom
+ * Author URI:        https://example.com
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       mon-plugin-meteo
+ */
